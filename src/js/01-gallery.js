14674b3ecc4049b97bb9a92723f1e6b2f7668643
IgorKryvenko56 <ikryvenko@yahoo.co.uk>
2023-04-12 16:40:15 +0300

    JS-08-new2 libraries added

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,7 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
 
 console.log(galleryItems);
