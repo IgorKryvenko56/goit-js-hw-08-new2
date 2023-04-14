a8fe48361c190344e5e9c3d091e50873a355cc79
IgorKryvenko56 <ikryvenko@yahoo.co.uk>
2023-04-14 14:24:10 +0300

    JS-HW-08new2 corrections done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,29 +1,38 @@
-// Add imports above this line
-import SimpleLightbox from "simplelightbox";
-import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
-import { galleryItems } from './gallery-items';
-// Change code below this line
+ // Add imports above this line
+ import SimpleLightbox from "simplelightbox";
+ import "simplelightbox/dist/simple-lightbox.min.css"; // Import the CSS file
+ import { galleryItems } from './gallery-items';
 
+ // Change code below this line
  const gallery = document.querySelector('.gallery');
-
-  const imgs = galleryItems.map(item => `
-  <li class='gallery__item'>
-  <a class='gallery__link' href='${item.original}'>
-  <img class='gallery__image' src='${item.preview}'
-  data-source='${item.original}' alt='${item.description}' /></a>
-  </li>`).join('');
-
+ const imgs = galleryItems.map(item => `
+   <li class='gallery__item'>
+     <a class='gallery__link' href='${item.original}'>
+      <img class='gallery__image' src='${item.preview}'
+ data-source='${item.original}' alt='${item.description}' />
+     </a>
+   </li>
+`).join('');
  gallery.insertAdjacentHTML('beforeend', imgs);
+
  gallery.addEventListener('click', (evt) => {
-   evt.preventDefault();
-    if (evt.target.classList.contains('gallery_image')) {
-       const imgSrc = evt.target.dataset.source;
-       console.log(imgSrc);
-    }
-  });
+  evt.preventDefault();
+  if (evt.target.classList.contains('gallery__image')) {
+     const imgSrc = evt.target.dataset.source;
+    console.log(imgSrc);
+   }
+ });
 
-const galleryS = new SimpleLightbox('.gallery a', {   
-  captionsData: 'alt',   captionDelay: 250,
-});
+ const galleryS = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+ });
 
-console.log(galleryItems);
+ console.log(galleryItems);
+
+
+
+
+
+
+
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,17 +1,24 @@
-import Player from '@vimeo/player';
-import throttle from 'lodash.throttle';
+ import Player from '@vimeo/player';
+ import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-const data = function (data) {
-  localStorage.setItem('videoplayer-current-time', Math.floor(data.seconds));
+// Функция для сохранения текущего времени в local storage
+const saveCurrentTime = function () {
+  player.getCurrentTime().then(function (seconds) {
+  localStorage.setItem('videoplayer-current-time', Math.floor(seconds));
+  });
 };
+// Каждую секунду фротлим
+const throttledSave = throttle(saveCurrentTime, 1000);
+// применяем метод on() и вызываем throttledSave function
+player.on('timeupdate', throttledSave);
 
-
-player.on('timeupdate', throttle(data, 1000));
-
-const timeBeforeFailure = 
-  localStorage.getItem('videoplayer-current-time');
-
-player.setCurrentTime(timeBeforeFailure);
+// достаем сохраненное время проигывания из local storage и продолжаем с этого момента
+const savedTime = localStorage.getItem('videoplayer-current-time');
+if (savedTime) {
+  player.setCurrentTime(savedTime).then(function () {
+    console.log('Воспроизведение возобновлено  сохраненной позиции:', savedTime);
+  });
+}
